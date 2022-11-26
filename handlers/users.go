f746ed644509c3a8eeec6435aa02f4909647a014
Moh Dzulfikri Maulana <dzuuuuul2@gmail.com>
2022-11-26 08:10:40 +0700

    simplify code

--- handlers/users.go
+++ handlers/users.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 	dto "ways-bucks-api/dto/result"
@@ -10,7 +9,6 @@ import (
 	"ways-bucks-api/models"
 	"ways-bucks-api/repositories"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
@@ -58,92 +56,6 @@ func (h *handler) GetUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	response := dto.SuccessResult{Status: "success", Data: convertResponse(user)}
-	json.NewEncoder(w).Encode(response)
-}
-
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	request := new(usersdto.CreateUserRequest)
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	validation := validator.New()
-	err := validation.Struct(request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	user := models.User{
-		Fullname: request.Fullname,
-		Email:    request.Email,
-		Password: request.Password,
-		Image:    request.Image,
-	}
-
-	data, err := h.UserRepository.CreateUser(user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-	}
-
-	fmt.Println(data)
-
-	w.WriteHeader(http.StatusOK)
-	response := dto.SuccessResult{Status: "success", Data: convertResponse(data)}
-	json.NewEncoder(w).Encode(response)
-}
-
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	request := new(usersdto.UpdateUserRequest)
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
-
-	user := models.User{}
-
-	user.ID = id
-
-	if request.Fullname != "" {
-		user.Fullname = request.Fullname
-	}
-
-	if request.Email != "" {
-		user.Email = request.Email
-	}
-
-	if request.Password != "" {
-		user.Password = request.Password
-	}
-
-	if request.Image != "" {
-		user.Image = request.Image
-	}
-
-	data, err := h.UserRepository.UpdateUser(user, id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	response := dto.SuccessResult{Status: "success", Data: convertResponse(data)}
 	json.NewEncoder(w).Encode(response)
 }
 
--- repositories/users.go
+++ repositories/users.go
@@ -9,13 +9,7 @@ import (
 type UserRepository interface {
 	FindUsers() ([]models.User, error)
 	GetUser(ID int) (models.User, error)
-	CreateUser(user models.User) (models.User, error)
-	UpdateUser(user models.User, ID int) (models.User, error)
 	DeleteUser(user models.User, ID int) (models.User, error)
-}
-
-type repository struct {
-	db *gorm.DB
 }
 
 func RepositoryUser(db *gorm.DB) *repository {
@@ -24,7 +18,8 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
-	err := r.db.Find(&users).Error // err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
+	// err := r.db.Find(&users).Error // err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
+	err := r.db.Preload("Profile").Preload("Products").Find(&users).Error
 
 	return users, err
 }
@@ -32,18 +27,6 @@ func (r *repository) FindUsers() ([]models.User, error) {
 func (r *repository) GetUser(ID int) (models.User, error) {
 	var user models.User
 	err := r.db.First(&user, ID).Error // err := r.db.Raw("SELECT * FROM users WHERE id=?", ID).Scan(&user).Error
-
-	return user, err
-}
-
-func (r *repository) CreateUser(user models.User) (models.User, error) {
-	err := r.db.Create(&user).Error // 	err := r.db.Exec("INSERT INTO users(fullname,email,password,image) VALUES (?,?,?,?)", user.Fullname, user.Email, user.Password, user.Image).Error
-
-	return user, err
-}
-
-func (r *repository) UpdateUser(user models.User, ID int) (models.User, error) {
-	err := r.db.Save(&user).Error // err := r.db.Raw("UPDATE users SET fullname=?, email=?, password=?, image=? WHERE id=?", user.Fullname, user.Email, user.Password, user.Image, ID).Scan(&user).Error
 
 	return user, err
 }
