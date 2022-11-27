27f35eeb3a1aa1f3205e82f912d43e40b764ed57
Moh Dzulfikri Maulana <dzuuuuul2@gmail.com>
2022-11-27 13:29:50 +0700

    add Transaction model

---
+++ models/transaction.go
@@ -0,0 +1,11 @@
+package models
+
+type Transaction struct {
+	ID        int                  `json:"id" gorm:"primary_key:auto_increment"`
+	ProductID int                  `json:"product_id"`
+	Product   ProductResponse      `json:"product"`
+	BuyerID   int                  `json:"buyer_id"`
+	Buyer     UsersProfileResponse `json:"buyer"`
+	Price     int                  `json:"price"`
+	Status    string               `json:"status"  gorm:"type:varchar(25)"`
+}
--- repositories/users.go
+++ repositories/users.go
@@ -18,8 +18,8 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
-	// err := r.db.Find(&users).Error // err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
-	err := r.db.Preload("Products").Find(&users).Error
+	err := r.db.Find(&users).Error // err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
+	// err := r.db.Preload("Products").Find(&users).Error
 
 	return users, err
 }
